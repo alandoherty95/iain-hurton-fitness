eadbec49192e49576c37cd482d5251113cb586d3
alandoherty95 <alandoherty95@gmail.com>
2021-05-15 12:37:45 +0000

    Created base template and index.html

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, url_for)
 if os.path.exists("env.py"):
     import env
 
@@ -8,8 +10,9 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/index")
+def home():
+    return render_template("index.html", home=home)
 
 
 if __name__ == "__main__":
