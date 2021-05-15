2d9524f199963d5ad46be0ecc3d073cbce87f215
alandoherty95 <alandoherty95@gmail.com>
2021-05-15 16:40:26 +0000

    Created pages for about, contact, gallery and events

--- app.py
+++ app.py
@@ -15,6 +15,26 @@ def home():
     return render_template("index.html", home=home)
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html", about=about)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", contact=contact)
+
+
+@app.route("/events")
+def events():
+    return render_template("events.html", events=events)
+
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html", gallery=gallery)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav({
-        edge: "right"
+    $('.sidenav').sidenav({ // naviagtion bar
+        edge: "right" // side naviagtion bar opens on right
     });
+    $('.carousel').carousel({});
   });
