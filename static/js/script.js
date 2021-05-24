47e7ca06f533d0c478c0ba67f98ce8362d046f5e
alandoherty95 <alandoherty95@gmail.com>
2021-05-24 21:19:05 +0000

    Added contact details, about page improved

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,7 @@ $(document).ready(function () {
     $('.sidenav').sidenav({ // naviagtion bar
         edge: "right" // side naviagtion bar opens on right
     });
-    $('.carousel').carousel({});
+    $('.carousel').carousel({
+        numVisible: 3
+    });
 });
